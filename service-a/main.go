a7c5c7eec1d5ec59f7a5931bcd2fe69a778ed403
ravjot07 <ravu2004@gmail.com>
2025-03-26 18:43:24 +0530

    feat(service-a): implement basic HTTP server with /hello endpoint
    Signed-off-by: ravjot07 <ravu2004@gmail.com>

---
+++ service-a/main.go
@@ -0,0 +1,28 @@
+package main 
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+// helloHandler responds with a greeting message.
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	message := "Hello from Service A form octo!"
+	fmt.Fprintln(w, message)
+}
+
+func main(){
+	
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/hello", helloHandler)
+	log.Printf("Service A: Starting server on port %s...\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
